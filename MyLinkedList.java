a750b7c75f4acbf5f82fec0f91d29f13fb0ba33c
ElizaHlglh <ewu4@stuy.edu>
2018-12-12 22:15:05 -0500

    Node.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,19 +2,27 @@ public class MyLinkedList{
     private int size;
     private Node start,end;
 
-    public MyLinkedList{
-
+    public MyLinkedList(int value, int begin, int last){
+      size = value;
+      start = new Node(begin);
+      end = new Node(last);
+      //considering something like subString: Node = (index in the list, data, index+1)
     }
 
     public int size(){
-	return size;
+      return size;
     }
 
     public boolean add(int value){
-	end = new Node(value);
+      end = new Node(value);
+      return true;
     }
 
     public String toString(){
+      String ans = "";
+      for (int i=0; i < size; i++){
 
+      }
+      return
     }
 }
--- Node.java
+++ Node.java
@@ -1,12 +1,20 @@
 public class Node{
     private int data;
     private Node next,prev;
-    public Node(int now) {
-	data = now;
-    }
-    public Node(int before, int after){
-	prev = new Node(before);
-	next = new Node(after);
 
+    public Node(int now) {
+      data = now;
     }
+
+    public Node(int now, int after){
+      next = new Node(after);
+      data = now;
+    }
+
+    public Node(int before, int now, int after){
+      prev = new Node(before);
+      next = new Node(after);
+      data = now;
+    }
+
 }
