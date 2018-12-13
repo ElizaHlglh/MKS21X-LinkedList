a32759a8a3ea124a6b7332fadee9ad36dbf7719a
ElizaHlglh <ewu4@stuy.edu>
2018-12-13 11:44:40 -0500

    MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,11 +2,8 @@ public class MyLinkedList{
     private int size;
     private Node start,end;
 
-    public MyLinkedList(int value, int begin, int last){
-      size = value;
-      start = new Node(begin);
-      end = new Node(last);
-      //considering something like subString: Node = (index in the list, data, index+1)
+    public MyLinkedList(){
+      size = 0;
     }
 
     public int size(){
