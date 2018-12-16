0387c97a7384e0778a5f375694c15fc8515bd047
ElizaHlglh <ewu4@stuy.edu>
2018-12-15 21:57:43 -0500

    Node.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -39,11 +39,16 @@ public class MyLinkedList{
       return ans;
     }
 
-    private Integer get(int index){
+    private Node getNode(int index){
       Node current = start;
       for (int i = 0; i < index && current != null; i++){
         current = current.next();
       }
+      return current;
+    }
+
+    public Integer get(int index){
+      Node current = getNode(index);
       if (current != null){
         return current.getData();
       }
@@ -52,17 +57,34 @@ public class MyLinkedList{
       }
     }
 
-    private Integer set(int index, Integer value){
-      Node current = start;
-      While (current.next() != null){
-        if (current.getData() == )
-      }
+    public Integer set(int index, Integer value){
+      Node ToChange = getNode(index);
+      return ToChange.setData(value);
     }
 
     public boolean contains(Integer value){
       Node current = start;
-      While (current.next() != null){
-        if (current.getData() == )
+      while (current.next() != null){
+        if (current.getData() == value){
+          return true;
+        }
+        current = current.next();
       }
+      return false;
     }
+
+    public int indexOf(Integer value){
+      Node current = start;
+      int count = 0;
+      while (current.next() != null){
+        if (current.getData() == value){
+          return count;
+        }
+        count++;
+      }
+      return -1;
+    }
+
+    
+
 }
