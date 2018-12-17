0806cc6b1abd483cd26c5f797ce282ef4a2f5e12
ElizaHlglh <ewu4@stuy.edu>
2018-12-16 21:22:22 -0500

    MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -30,13 +30,13 @@ public class MyLinkedList{
     }
 
     public String toString(){
-      String ans = "";
+      String ans = "{";
       Node current = start;
       while(current != null){
         ans += current.getData();
         current = current.next();
       }
-      return ans;
+      return ans+"}";
     }
 
     private Node getNode(int index){
@@ -80,11 +80,48 @@ public class MyLinkedList{
         if (current.getData() == value){
           return count;
         }
+        current = current.next();
         count++;
       }
       return -1;
     }
 
-    
+    public void add(int index, Integer value){
+      Node before = getNode(index-1);
+      Node ToAdd = new Node(value);
+      Node After = getNode(index);
+      before.setNext(ToAdd);
+      ToAdd.setPrev(before);
+      ToAdd.setNext(After);
+      After.setPrev(ToAdd);
+    }
+
+    public Integer remove(int index){
+      Node toRemove = getNode(index);
+      Node Before = getNode(index-1);
+      Node After = getNode(index+1);
+      Before.setNext(After);
+      After.setPrev(Before);
+      toRemove.setPrev(null);
+      toRemove.setNext(null);
+      return toRemove.getData();
+    }
+
+    public boolean remove(Integer value){
+      if (contains(value)){
+        int index = indexOf(value);
+        Node toRemove = getNode(index);
+        Node Before = getNode(index-1);
+        Node After = getNode(index+1);
+        Before.setNext(After);
+        After.setPrev(Before);
+        toRemove.setPrev(null);
+        toRemove.setNext(null);
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
 
 }
